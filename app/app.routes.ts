d29bf39538b6559780f9dcf5a4784c8eb6073d7f
Jennifer Minnich <jminniecc@gmail.com>
2017-02-15 10:44:40 -0700

    app/app.routes.ts & home.php

---
+++ app/app.routes.ts
@@ -0,0 +1,14 @@
+//all components go here
+
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./components/home-component";
+
+export const allAppComponents = [HomeComponent];
+
+export const routes: Routes = [
+	{path: "", component: HomeComponent}
+];
+
+export const appRoutingProviders: any[] = [];
+
+export const routing = RouterModule.forRoot(routes);
